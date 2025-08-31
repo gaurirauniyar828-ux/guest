be4c162cf2bd28cc47ce78482a45db0ebba3b302
GauriRauniyar <gaurirauniyar828.com>
2025-08-31 11:52:08 +0545

    login page

---
+++ guest.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>login guest</h2>
+    <label for="">name</label>
+    <input type="text" name="name" id="name">
+    <label for="">email</label>
+    <input type="email" name="email" id="email">
+    <label for="">password</label>
+    <input type="password" name="password" id="password"> 
+    <br>
+    <input type="submit" value="login">
+</body>
+</html>
