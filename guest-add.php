3f1efb77216889d4c751d177352a1e7a7f407403
xaofao <ceemran.4@gmail.com>
2025-08-31 11:41:53 +0545

    form

---
+++ guest-add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+
+        <h2>Guest Book Entry</h2>
+                <div class="form-container"></div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+        </div>
+                <div class="form-container">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        </div>
+                <div class="form-container">
+        <label for="message">Message:</label><br>
+        <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
+        </div>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
