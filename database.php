e4189220b3505fc9ceee09093ce19c37c9bed234
xaofao <ceemran.4@gmail.com>
2025-08-31 12:06:55 +0545

    updated

---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "guest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+if(isset($_POST['add_guest'])){
+  $name=$_POST['name']??'';
+  $email=$_POST['email']??'';
+  $number=$_POST['number']??'';
+  $message=$_POST['message']??'';
+
+
+$sql = "INSERT INTO `list-guest` (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
